c50affc52b4f6f8526554d3e4a895118c77c7830
Iosebi Gobronidze <gobronidze.ioseb@gmail.com>
2017-02-02 18:14:43 +0400

    added system user create sql...

---
+++ src/java/ge/mziuri/resources/sql/SystemUserCreate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE SystemUser (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+    );
